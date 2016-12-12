b3acf07dbbe822b755d63f169d0ee8dcf95a04c1
AlessandraH <alehairi@gmail.com>
2016-12-12 09:49:52 -0200

    G_Funcionario atualizado para novo BD

--- Ribanceira/src/controladores/G_Funcionario.java
+++ Ribanceira/src/controladores/G_Funcionario.java
@@ -17,11 +17,11 @@ import org.hibernate.Session;
  */
 public class G_Funcionario {
     
-    public void salvaFuncionario(String nome, String dataNascimento, String endereco, String telefone, String email, String rg, String cpf, String tituloEleitor, String estadoCivil, String numeroCarteiraTrabalho) {
+    public void salvaFuncionario(String nome, String dataNascimento, String endereco, String telefone, String email, String rg, String cpf, String tituloEleitor, String estadoCivil, String numeroCarteiraTrabalho, boolean ativo) {
         
         Session s = HibernateUtil.getSessionFactory().getCurrentSession();
         s.beginTransaction();
-        s.save(new Funcionario(nome, dataNascimento, endereco, telefone, email, rg, cpf, tituloEleitor, estadoCivil, numeroCarteiraTrabalho));
+        s.save(new Funcionario(nome, dataNascimento, endereco, telefone, email, rg, cpf, tituloEleitor, estadoCivil, numeroCarteiraTrabalho, ativo));
         s.getTransaction().commit();
         
     }
@@ -39,6 +39,21 @@ public class G_Funcionario {
         s.getTransaction().commit();
         return listaFuncionario;
     }
+    
+    public ArrayList<Funcionario> getListaIdFuncionario() {
+        Session s = HibernateUtil.getSessionFactory().getCurrentSession();
+        s.beginTransaction();
+        ArrayList<Funcionario> listaFuncionario = (ArrayList<Funcionario>) s.createQuery("From Funcionario").list();
+        Collections.sort(listaFuncionario, new Comparator<Funcionario>() {
+            @Override
+            public int compare(Funcionario o1, Funcionario o2) {
+                return o1.getCodigo().compareTo(o2.getCodigo());
+            }
+        });
+        s.getTransaction().commit();
+        return listaFuncionario;
+    }
+    
     
     public void atualizaFuncionario(Funcionario f) {
         
